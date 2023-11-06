078b23bbec91de4a0167645b95dab44bc6f75d14
Souvik chakraborty <22souvik22@gmail.com>
2023-11-06 22:55:20 +0530

    More routes added

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/TitleForm.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/TitleForm.tsx
@@ -16,6 +16,9 @@ import { input } from "zod";
 import { Pencil } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 
 interface TitleFormProps {
@@ -32,7 +35,12 @@ const formSchema = z.object({
 })
 
 export default function TitleForm({ initialData, courseId }: TitleFormProps) {
+    const router = useRouter();
+
     const [isEditing, setisEditiing] = useState(false)
+    function toggleEdit() {
+        setisEditiing((current)=>!current)
+    }
 
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -42,7 +50,14 @@ export default function TitleForm({ initialData, courseId }: TitleFormProps) {
     const { isSubmitting, isValid } = form.formState;
 
     async function onSubmit(values: z.infer<typeof formSchema>) {
-        console.log(values);
+        try {
+            await axios.patch(`/api/courses/${courseId}`,values);
+            toast.success("Course updated");
+            toggleEdit();
+            router.refresh();
+        } catch (error) {
+            toast.error("Something went wrong")
+        }
 
     }
 
@@ -50,13 +65,12 @@ export default function TitleForm({ initialData, courseId }: TitleFormProps) {
         <div className="mt-6 bg-slate-100 rounded-md p-4">
             <div className="font-medium flex items-center justify-between">
                 Course Title
-                <Button variant="ghost">
-                    {isEditing && (
+                <Button onClick={toggleEdit} variant="ghost">
+                    {isEditing? (
                         <>
                             Cancle
                         </>
-                    )}
-                    {!isEditing && (
+                    ):(
                         <>
                             <Pencil className="h-4 w-4 mr-2" />
                             Edit Title
@@ -64,6 +78,40 @@ export default function TitleForm({ initialData, courseId }: TitleFormProps) {
                     )}
                 </Button>
             </div>
+            {isEditing && (
+                <p className="text-sm mt-2">
+                    {initialData.title}
+                </p>
+            )}
+            {isEditing && (
+                <Form {...form}>
+                    <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="space-y-4 mt-4">
+                        <FormField
+                            control={form.control}
+                            name="title"
+                            render={({field})=>(
+                                <FormItem>
+                                    <FormControl>
+                                        <Input
+                                            disabled={isSubmitting}
+                                            placeholder="e.g Advanced web development"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage/>
+                                </FormItem>
+                            )}
+                        />
+                        <div className="flex items-center gap-x-2">
+                            <Button disabled={!isValid || isSubmitting} type="submit">
+                                Save
+                            </Button>
+                        </div>
+                    </form>
+                </Form>
+            )}
         </div>
     )
 };
